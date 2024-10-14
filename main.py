1b725ade20d016910847b7f4e674eb65b5adba91
Jesus Macias <jesus.macias.amador@gmail.com>
2024-10-13 21:20:18 -0600

    init

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from mangum import Mangum
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/hello/{name}")
+async def say_hello(name: str):
+    return {"message": f"Hello {name}"}
+
+
+handler = Mangum(app)
