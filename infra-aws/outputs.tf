c7e256662f530bade0cf913c64e5c2ed4b3d355d
Jesus Macias <jesus.macias.amador@gmail.com>
2024-12-30 00:08:18 -0600

    fix: pipline

--- infra-aws/outputs.tf
+++ infra-aws/outputs.tf
@@ -3,17 +3,10 @@ output "s3_bucket_name" {
 }
 
 
-# Outputs para obtener información útil
 output "lambda_function_name" {
-  value = coalesce(
-    data.aws_lambda_function.existing_lambda[0].function_name,
-    aws_lambda_function.kuosel_lambda[0].function_name
-  )
+  value = var.lambda_already_exists ? data.aws_lambda_function.existing_lambda[0].function_name : aws_lambda_function.kuosel_lambda[0].function_name
 }
 
 output "lambda_function_arn" {
-  value = coalesce(
-    data.aws_lambda_function.existing_lambda[0].arn,
-    aws_lambda_function.kuosel_lambda[0].arn
-  )
+  value = var.lambda_already_exists ? data.aws_lambda_function.existing_lambda[0].arn : aws_lambda_function.kuosel_lambda[0].arn
 }
