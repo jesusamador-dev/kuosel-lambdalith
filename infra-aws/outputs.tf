ef2866faf28506d00a73f80ba5436431316ed167
Jesus Macias <jesus.macias.amador@gmail.com>
2024-12-30 00:41:33 -0600

    fix

--- infra-aws/outputs.tf
+++ infra-aws/outputs.tf
@@ -4,9 +4,15 @@ output "s3_bucket_name" {
 
 
 output "lambda_function_name" {
-  value = var.lambda_already_exists ? data.aws_lambda_function.existing_lambda[0].function_name : aws_lambda_function.kuosel_lambda[0].function_name
+  value = coalesce(
+    try(data.aws_lambda_function.existing_lambda[0].function_name, null),
+    try(aws_lambda_function.kuosel_lambda[0].function_name, null)
+  )
 }
 
 output "lambda_function_arn" {
-  value = var.lambda_already_exists ? data.aws_lambda_function.existing_lambda[0].arn : aws_lambda_function.kuosel_lambda[0].arn
+  value = coalesce(
+    try(data.aws_lambda_function.existing_lambda[0].arn, null),
+    try(aws_lambda_function.kuosel_lambda[0].arn, null)
+  )
 }
