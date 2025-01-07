7407a51f1a7c7af15c2e249241531991159806c3
Jesús Guadalupe Macías Amador <47731561+jesusamador-dev@users.noreply.github.com>
2025-01-07 11:35:56 -0600

    Update outputs.tf

--- infra-aws/outputs.tf
+++ infra-aws/outputs.tf
@@ -4,10 +4,13 @@ output "s3_bucket_name" {
 
 
 output "lambda_function_name" {
-  value = try(aws_lambda_function.kuosel_lambda.function_name, null)
+  value = can(data.aws_lambda_function.existing_lambda.function_name) ? 
+          data.aws_lambda_function.existing_lambda.function_name : 
+          aws_lambda_function.kuosel_lambda.function_name
 }
 
 output "lambda_function_arn" {
-  value = try(aws_lambda_function.kuosel_lambda.arn, null)
+  value = can(data.aws_lambda_function.existing_lambda.arn) ? 
+          data.aws_lambda_function.existing_lambda.arn : 
+          aws_lambda_function.kuosel_lambda.arn
 }
-
